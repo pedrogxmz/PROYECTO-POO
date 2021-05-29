a8321fbb00c33c7c773444d0d44ec472f7683075
[Pedro Gomez] <pedro_goap@outlook.com>
2021-05-29 18:47:51 -0500

    Añadido de archivos

---
+++ PrestamosPendientes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    class PrestamosPendientes
+    {
+        public  User user { get; }
+        public Book Book { get; }
+
+        public PrestamosPendientes(User usuario, Book book)
+        {
+            user = usuario;
+            Book = book;
+        }
+    }
+
+}
